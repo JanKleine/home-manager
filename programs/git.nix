b0b8463115adf1f1747c6035db70415172d67f25
Jan Kleine <janpaulkleine@icloud.com>
2024-12-28 23:56:49 +0100

    feat: add difftastic to git

--- programs/git.nix
+++ programs/git.nix
@@ -4,5 +4,6 @@
     aliases = {
       lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'";
     };
+    difftastic.enable = true;
   };
 }
