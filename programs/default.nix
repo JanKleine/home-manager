921a1f48b4862d0dcb477d38217132a6651c8059
Jan Kleine <janpaulkleine@icloud.com>
2024-12-29 17:42:56 +0100

    feat: add bat

--- programs/default.nix
+++ programs/default.nix
@@ -7,6 +7,7 @@
 
   # Simple program definitions
   programs.fzf.enable = true;
+  programs.bat.enable = true;
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
