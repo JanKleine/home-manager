a1b3cddd9ad4d02791569606283a98104629a24d
Jan Kleine <jan.kleine@ipt.ch>
2025-12-04 14:27:33 +0100

    fix(neovim): configure nvim-tree

--- programs/neovim/after/plugin/tree.lua
+++ programs/neovim/after/plugin/tree.lua
@@ -1,1 +1,10 @@
-require("nvim-tree").setup()
+local api = require("nvim-tree").setup({
+    actions = {
+        open_file = {
+            quit_on_open = true,
+        },
+    },
+})
+
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true })
+
