4da8abf1563af3b95a6fe877a58948f9dc8f8215
Jan Kleine <janpaulkleine@icloud.com>
2026-02-16 16:17:11 +0100

    fix(neovim): fix golsp config

--- programs/neovim/after/plugin/lsp.lua
+++ programs/neovim/after/plugin/lsp.lua
@@ -30,7 +30,7 @@ local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    ensure_installed = {'rust_analyzer', 'gopls'},
+    ensure_installed = {'rust_analyzer'},
     handlers = {
         function(server_name)
             require('lspconfig')[server_name].setup({
@@ -59,7 +59,8 @@ require('mason-lspconfig').setup({
         end,
         gopls = function()
             require('lspconfig').gopls.setup({
-                capabilities = {
+                capabilities = lsp_capabilities,
+                settings = {
                     gopls = {
                         analyses = {
                             unusedparams = true,
