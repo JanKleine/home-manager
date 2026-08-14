5b3a27b59d26cbe8062ff4b3221acf7927e535d5
Jan Kleine <jan.kleine@ipt.ch>
2026-08-14 15:58:28 +0200

    feat(neovim): add ? shortcut in file browser to show shortcuts

--- programs/neovim/after/plugin/tree.lua
+++ programs/neovim/after/plugin/tree.lua
@@ -1,4 +1,6 @@
-local api = require("nvim-tree").setup({
+local api = require("nvim-tree.api")
+
+require("nvim-tree").setup({
     actions = {
         open_file = {
             quit_on_open = true,
@@ -7,7 +9,14 @@ local api = require("nvim-tree").setup({
     view = {
         width = {},
     },
+    on_attach = function(bufnr)
+        api.config.mappings.default_on_attach(bufnr)
+
+        -- which-key popup of the tree's own mappings, like <leader> elsewhere
+        vim.keymap.set('n', '?', function()
+            require('which-key').show({ global = false })
+        end, { buffer = bufnr, noremap = true, desc = 'Show file tree keymaps' })
+    end,
 })
 
 vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, desc = 'Toggle file tree' })
-
